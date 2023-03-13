9f60b2adaf9d2e4636704df5458db5b3fea2df24
Double Faces <projects.manager@xdomain.it>
2023-03-13 08:47:01 +0100

    test modal with  event

--- pages/app/_status.vue
+++ pages/app/_status.vue
@@ -32,6 +32,42 @@
         exact
       >Tutti ({{ completedTasks.length }})</nuxt-link>      
     </div>
+    <transition name="fade">
+      <div v-if="isModalVisible">
+        <div @click="onToggle" class="absolute opacity-70 inset-0 z-0"></div>
+          <div class="w-full max-w-lg p-3 relative mx-auto my-auto rounded-xl shadow-lg bg-white">
+            <div>
+              <div class="text-center p-3 flex-auto justify-center leading-6">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-16 h-16 flex items-center text-purple-500 mx-auto"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <h2 class="text-2xl font-bold py-4">Are you sure?</h2>
+                <p class="text-md text-gray-500 px-8">Do you really want to exit without saving your work?</p>
+              </div>
+              <div class="p-3 mt-2 text-center space-x-4 md:block">
+              <button
+                class="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-md hover:shadow-lg hover:bg-gray-100"
+              >Save
+              </button>
+              <button
+                @click="onToggle"
+                class="mb-2 md:mb-0 bg-purple-500 border border-purple-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-md hover:shadow-lg hover:bg-purple-600"
+              >Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
     <nuxt-link 
         :class="[status === 'completed' ? 'pill-active' : 'pill-inactive']" 
         class="pill-default lg:flex-grow-0 no-underline" 
@@ -86,111 +122,121 @@ import LoadingButton from '@/components/LoadingButton'
 /*import TaskModel from '@/models/Task'*/
 
 export default {
-    components: {
-      Task,
-      NewTask,
-      LoadingButton      
-    },
-    data () {
-      return {
-        isLoading: false,
-        isRemoveLoading: false,
-        isAnimated: true,
-        initialTasks: [],
-        fakeTasks: [
-          {
-            id: 1,
-            title: 'Task 1',
-            is_completed: false
-          },
-          {
-            id: 2,
-            title: 'Task 2',
-            is_completed: true
-          },
-          {
-            id: 3,
-            title: 'Task 3',
-            is_completed: false
-          }
-        ]
-      }
-    },
-    computed: {
-      allTasks () {
-        return this.fakeTasks
-      },
-      activeTasks () {
-        return this.fakeTasks.filter(task => !task.is_completed)
-      },
-      completedTasks () {
-        return this.fakeTasks.filter(task => task.is_completed)
-      },
-      filteredTasks () {
-        switch (this.status) {
-          case 'active':
-            return this.activeTasks
-          case 'completed':
-            return this.completedTasks
-          default:
-            return this.allTasks
+  components: {
+    Task,
+    NewTask,
+    LoadingButton      
+  },
+  data () {
+    return {
+      isOpen: false,
+      isLoading: false,
+      isRemoveLoading: false,
+      isAnimated: true,
+      initialTasks: [],
+      fakeTasks: [
+        {
+          id: 1,
+          title: 'Task 1',
+          is_completed: false
+        },
+        {
+          id: 2,
+          title: 'Task 2',
+          is_completed: true
+        },
+        {
+          id: 3,
+          title: 'Task 3',
+          is_completed: false
         }
-      },
-      status () {
-        console.log(this.$route.params.status)
-        return this.$route.params.status || 'all'
-      },
-      tasks () {
-        if (this.isAnimated) {
-          return this.initialTasks
-        }
-        return this.filteredTasks
-      },
-      timeToChill () {
-        return this.tasks.length === 0
-      },
-      storageTasks() {
-        return this.getLocalStorageArray('tasks')
-      },
-    },
-    /*
-    created() {
-      const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-      this.$store.commit('setTasks', tasks);
-      window.addEventListener('storage', this.handleStorageEvent);
-    },
-    beforeDestroy() {
-      window.removeEventListener('storage', this.handleStorageEvent);
-    },
-    methods: {
-      handleStorageEvent(event) {
-        if (event.key === 'tasks') {
-          const tasks = JSON.parse(event.newValue) || [];
-          this.$store.commit('setTasks', tasks);
-        }
-      }
+      ]
     }
-    */
-    created: function() {
-      this.setTasks()
+  },
+  mounted: function(){
+    //this.onToggle()
+    this.$root.$on('my-event', (payload) => {
+      console.log('yes', payload)
+      this.onToggle()
+      
+    })  
+  },
+  computed: {
+    isModalVisible() {
+      return this.isOpen;
     },
-    methods: {
-      async deleteTasks () {
-        this.isRemoveLoading = true
-        const completedTasks = this.completedTasks.map(task => task.id)
-        try {
-          await this.$store.dispatch('entities/tasks/remove', completedTasks)
-        } catch (error) {
-          console.log(error)
-        } finally {
-          this.isRemoveLoading = false
-        }
-      },
-      async setTasks () {        
-        this.$store.commit('setTodos', this.storageTasks)
+    allTasks () {
+      return this.fakeTasks
+    },
+    activeTasks () {
+      return this.fakeTasks.filter(task => !task.is_completed)
+    },
+    completedTasks () {
+      return this.fakeTasks.filter(task => task.is_completed)
+    },
+    filteredTasks () {
+      switch (this.status) {
+        case 'active':
+          return this.activeTasks
+        case 'completed':
+          return this.completedTasks
+        default:
+          return this.allTasks
       }
-
+    },
+    status () {
+      console.log(this.$route.params.status)
+      return this.$route.params.status || 'all'
+    },
+    tasks () {
+      if (this.isAnimated) {
+        return this.initialTasks
+      }
+      return this.filteredTasks
+    },
+    timeToChill () {
+      return this.tasks.length === 0
+    },
+    storageTasks() {
+      return this.getLocalStorageArray('tasks')
+    },
+  },
+  created() {
+    //const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const tasks = this.getLocalStorageArray('tasks') || []
+    this.$store.commit('setTasks', tasks)
+    window.addEventListener('storage', this.handleStorageEvent)
+    window.addEventListener('storage', console.log('test 1'))
+  },
+  beforeDestroy() {
+    window.removeEventListener('storage', this.handleStorageEvent)
+  },
+  methods: {
+    onToggle() {
+      this.isOpen = !this.isOpen;
+    },
+    handleStorageEvent(event) {
+      console.log('hi it works!')
+      if (event.key === 'tasks') {
+        const tasks = JSON.parse(event.newValue) || []
+        this.$store.commit('setTasks', tasks)
+      }
+    },
+    async deleteTasks () {
+      this.isRemoveLoading = true
+      const completedTasks = this.completedTasks.map(task => task.id)
+      try {
+        await this.$store.dispatch('entities/tasks/remove', completedTasks)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        this.isRemoveLoading = false
+      }
+    },
+    async setTasks () {        
+      this.$store.commit('setTodos', this.storageTasks)
     }
+  }
 }
 </script>
 
